e1ae51b511176fb277aa819a99b6e0128c646d28
Davor Babic <davor@davor.se>
2024-07-26 23:43:36 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Common Swedish Words -- Flash Card App
+
+This repo allows you to practice the most common Swedish words.
+It's basically a flash card app that I built to help people
+learn Swedish.
+
+## Demo
+
+You can see the app in action [here](https://common-swedish-words.davor.se/).
+
+## How to run
+
+```sh
+npm install
+npm run dev
+```
---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState, useCallback } from "react";
+import "./App.css";
+import Words from "./assets/swedish_words.json";
+
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function App() {
+  const [isFlipped, setIsFlipped] = useState(false);
+  const handleClick = useCallback(() => {
+    if (isFlipped) {
+      setTimeout(() => setWord(getRandomElement(Words)), 600);
+    }
+    setIsFlipped((state) => !state);
+  }, [isFlipped]);
+
+  const [word, setWord] = useState(getRandomElement(Words));
+  const swedishWord = word.swedish;
+  const swedishExample = word.example;
+  const englishWord = word.english;
+
+  return (
+    <div className="container">
+      <div
+        className={"box card " + (isFlipped && "is-flipped")}
+        id="box"
+        onClick={handleClick}
+      >
+        <div className="box__face box__face--front">{englishWord}</div>
+        <div className="box__face box__face--back">
+          <b>{swedishWord}</b>
+          {swedishExample}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
